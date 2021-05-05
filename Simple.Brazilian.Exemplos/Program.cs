e8d220a32d9543661b0ac32bd7e1dfc653225b91
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-05-05 17:41:54 -0300

    Criados projetos em branco

---
+++ Simple.Brazilian.Exemplos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simple.Brazilian.Exemplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, mundo !");
+        }
+    }
+}
