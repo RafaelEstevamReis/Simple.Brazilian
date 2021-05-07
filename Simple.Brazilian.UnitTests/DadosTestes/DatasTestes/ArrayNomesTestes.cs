bf3668ce4b7fa71658b708f2cd6005814e26bae8
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-05-07 18:19:56 -0300

    Adiciona arrays de dias da semana e nomes dos meses

---
+++ Simple.Brazilian.UnitTests/DadosTestes/DatasTestes/ArrayNomesTestes.cs
@@ -0,0 +1,76 @@
+﻿using Simple.Brazilian.Dados;
+using Xunit;
+
+namespace Simple.Brazilian.UnitTests.DadosTestes.DatasTestes
+{
+    public class ArrayNomesTestes
+    {
+        [Fact]
+        public void Datas_ArraysNomes_CertificaComprimento_Semana()
+        {
+            Assert.Equal(7, Datas.DiasSemanaComAcento.Length);
+            Assert.Equal(7, Datas.DiasSemanaSemAcento.Length);
+            Assert.Equal(7, Datas.DiasSemanaComAcentoL.Length);
+            Assert.Equal(7, Datas.DiasSemanaSemAcentoL.Length);
+            Assert.Equal(7, Datas.DiasSemanaComAcentoU.Length);
+            Assert.Equal(7, Datas.DiasSemanaSemAcentoU.Length);
+        }
+
+        [Fact]
+        public void Datas_ArraysNomes_CertificaComprimento_Meses()
+        {
+            Assert.Equal(12, Datas.MesesComAcento.Length);
+            Assert.Equal(12, Datas.MesesSemAcento.Length);
+            Assert.Equal(12, Datas.MesesComAcentoL.Length);
+            Assert.Equal(12, Datas.MesesSemAcentoL.Length);
+            Assert.Equal(12, Datas.MesesComAcentoU.Length);
+            Assert.Equal(12, Datas.MesesSemAcentoU.Length);
+        }
+
+        [Fact]
+        public void Datas_ArraysNomes_CertificaIgualdade_Semana()
+        {
+            var baseCompara = Datas.DiasSemanaSemAcentoL;
+
+            var todos = new string[][]{
+                Datas.DiasSemanaComAcento,
+                Datas.DiasSemanaSemAcento,
+                Datas.DiasSemanaComAcentoL,
+                //Datas.DiasSemanaSemAcentoL, - Já é a base
+                Datas.DiasSemanaComAcentoU,
+                Datas.DiasSemanaSemAcentoU,
+            };
+
+            foreach (var conjunto in todos)
+            {
+                for (int i = 0; i < baseCompara.Length; i++)
+                {
+                    Assert.Equal(baseCompara[i], Formatadores.Texto.RemoveAcentos(conjunto[i]).ToLower());
+                }
+            }
+        }
+
+        [Fact]
+        public void Datas_ArraysNomes_CertificaIgualdade_Meses()
+        {
+            var baseCompara = Datas.MesesSemAcentoL;
+
+            var todos = new string[][]{
+                Datas.MesesComAcento,
+                Datas.MesesSemAcento,
+                Datas.MesesComAcentoL,
+                //Datas.MesesSemAcentoL, - Já é a base
+                Datas.MesesComAcentoU,
+                Datas.MesesSemAcentoU,
+            };
+
+            foreach (var conjunto in todos)
+            {
+                for (int i = 0; i < baseCompara.Length; i++)
+                {
+                    Assert.Equal(baseCompara[i], Formatadores.Texto.RemoveAcentos(conjunto[i]).ToLower());
+                }
+            }
+        }
+    }
+}
---
+++ Simple.Brazilian/Dados/Datas.cs
@@ -0,0 +1,26 @@
+﻿namespace Simple.Brazilian.Dados
+{
+    public static class Datas
+    {
+        /* Normais */
+        public static readonly string[] MesesComAcento = { "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" };
+        public static readonly string[] MesesSemAcento = { "Janeiro", "Fevereiro", "Marco", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" };
+
+        public static readonly string[] DiasSemanaComAcento = { "Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado" };
+        public static readonly string[] DiasSemanaSemAcento = { "Domingo", "Segunda", "Terca", "Quarta", "Quinta", "Sexta", "Sabado" };
+        
+        /* Em MAIÚSCULO */
+        public static readonly string[] MesesComAcentoU = { "JANEIRO", "FEVEREIRO", "MARÇO", "ABRIL", "MAIO", "JUNHO", "JULHO", "AGOSTO", "SETEMBRO", "OUTUBRO", "NOVEMBRO", "DEZEMBRO" };
+        public static readonly string[] MesesSemAcentoU = { "JANEIRO", "FEVEREIRO", "MARCO", "ABRIL", "MAIO", "JUNHO", "JULHO", "AGOSTO", "SETEMBRO", "OUTUBRO", "NOVEMBRO", "DEZEMBRO" };
+
+        public static readonly string[] DiasSemanaComAcentoU = { "DOMINGO", "SEGUNDA", "TERÇA", "QUARTA", "QUINTA", "SEXTA", "SÁBADO" };
+        public static readonly string[] DiasSemanaSemAcentoU = { "DOMINGO", "SEGUNDA", "TERCA", "QUARTA", "QUINTA", "SEXTA", "SABADO" };
+
+        /* Em minúsculo */
+        public static readonly string[] MesesComAcentoL = { "janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro" };
+        public static readonly string[] MesesSemAcentoL = { "janeiro", "fevereiro", "marco", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro" };
+
+        public static readonly string[] DiasSemanaComAcentoL = { "domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado" };
+        public static readonly string[] DiasSemanaSemAcentoL = { "domingo", "segunda", "terca", "quarta", "quinta", "sexta", "sabado" };
+    }
+}
