11a06a3059a661c2ceb8090e5b71700addd8523b
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-05-08 19:36:59 -0300

    Adiciona camada de compatibilidade

---
+++ Simple.Brazilian/Compatibilidade/NetStd10.cs
@@ -0,0 +1,15 @@
+﻿#if NETSTANDARD1_0
+using System;
+
+namespace Simple.Brazilian
+{
+    public class DescriptionAttribute : Attribute
+    {
+        public string Description { get; }
+        public DescriptionAttribute(string description)
+        {
+            Description = description;
+        }
+    }
+}
+#endif
