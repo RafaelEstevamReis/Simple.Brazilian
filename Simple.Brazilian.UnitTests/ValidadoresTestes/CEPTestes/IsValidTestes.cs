b5c054589155f6eaa2a48f22cae776fef71007b4
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-05-09 00:44:19 -0300

    Adiciona validação de CEP

---
+++ Simple.Brazilian.UnitTests/ValidadoresTestes/CEPTestes/IsValidTestes.cs
@@ -0,0 +1,110 @@
+﻿using Simple.Brazilian.Validadores;
+using System;
+using System.Linq;
+using System.Reflection.Emit;
+using Xunit;
+
+namespace Simple.Brazilian.UnitTests.ValidadoresTestes.CEPTestes
+{
+    public class IsValidTestes
+    {
+        /// <summary>
+        /// Verifica o comprimento do CEP
+        /// </summary>
+        /// <param name="cep"></param>
+        /// <param name="resultado"></param>
+        [Theory]
+        [InlineData("", false)]
+        [InlineData("00.000", false)]
+        [InlineData("00.000-000", true)]
+        [InlineData("00000000", true)]
+        public void CEP_IsValid(string cep, bool resultado)
+        {
+            Assert.Equal(resultado, CEP.IsValid(cep));
+        }
+        /// <summary>
+        /// Varre todos os inícios de CEP para procurar brechas nos UFs
+        /// </summary>
+        [Fact]
+        public void CEP_IsValidVarreduraInicio()
+        {
+            for (char i = '0'; i <= '9'; i++)
+            {
+                var cep = $"{i}0.000-000";
+                foreach (var uf in Enum.GetValues<Dados.Locais.UF>())
+                {
+                    // Nãodeve lançar exceções
+                    CEP.IsValid(cep, uf);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Varre todas as UFs para procurar brechas nos CEPs
+        /// </summary>
+        [Theory]
+        [InlineData(Dados.Locais.UF.SP, '0')]
+        [InlineData(Dados.Locais.UF.SP, '1')]
+        [InlineData(Dados.Locais.UF.RJ, '2')]
+        [InlineData(Dados.Locais.UF.ES, '2')]
+        [InlineData(Dados.Locais.UF.MG, '3')]
+        [InlineData(Dados.Locais.UF.BA, '4')]
+        [InlineData(Dados.Locais.UF.SE, '4')]
+        [InlineData(Dados.Locais.UF.PE, '5')]
+        [InlineData(Dados.Locais.UF.AL, '5')]
+        [InlineData(Dados.Locais.UF.PB, '5')]
+        [InlineData(Dados.Locais.UF.RN, '5')]
+        [InlineData(Dados.Locais.UF.CE, '6')]
+        [InlineData(Dados.Locais.UF.PI, '6')]
+        [InlineData(Dados.Locais.UF.MA, '6')]
+        [InlineData(Dados.Locais.UF.PA, '6')]
+        [InlineData(Dados.Locais.UF.AP, '6')]
+        [InlineData(Dados.Locais.UF.AM, '6')]
+        [InlineData(Dados.Locais.UF.RR, '6')]
+        [InlineData(Dados.Locais.UF.AC, '6')]
+        [InlineData(Dados.Locais.UF.DF, '7')]
+        [InlineData(Dados.Locais.UF.GO, '7')]
+        [InlineData(Dados.Locais.UF.TO, '7')]
+        [InlineData(Dados.Locais.UF.MT, '7')]
+        [InlineData(Dados.Locais.UF.RO, '7')]
+        [InlineData(Dados.Locais.UF.MS, '7')]
+        [InlineData(Dados.Locais.UF.PR, '8')]
+        [InlineData(Dados.Locais.UF.SC, '8')]
+        [InlineData(Dados.Locais.UF.RS, '9')]
+        public void CEP_IsValidVarreduraUF(Dados.Locais.UF uf, char inicioCorreto)
+        {
+            var cep = $"{inicioCorreto}0.000-000";
+            Assert.True(CEP.IsValid(cep, uf));
+        }
+
+
+        /// <summary>
+        /// Varre todas as UFs para procurar brechas nos CEPs
+        /// </summary>
+        [Theory]
+        [InlineData('0', Dados.Locais.UF.SP)]
+        [InlineData('1', Dados.Locais.UF.SP)]
+        [InlineData('2', Dados.Locais.UF.RJ, Dados.Locais.UF.ES)]
+        [InlineData('3', Dados.Locais.UF.MG)]
+        [InlineData('4', Dados.Locais.UF.BA, Dados.Locais.UF.SE)]
+        [InlineData('5', Dados.Locais.UF.PE, Dados.Locais.UF.AL, Dados.Locais.UF.PB, Dados.Locais.UF.RN)]
+        [InlineData('6', Dados.Locais.UF.CE, Dados.Locais.UF.PI, Dados.Locais.UF.MA, Dados.Locais.UF.PA,
+                         Dados.Locais.UF.AP, Dados.Locais.UF.AM, Dados.Locais.UF.RR, Dados.Locais.UF.AC)]
+        [InlineData('7', Dados.Locais.UF.DF, Dados.Locais.UF.GO, Dados.Locais.UF.TO, Dados.Locais.UF.MT, 
+                         Dados.Locais.UF.RO, Dados.Locais.UF.MS)]
+        [InlineData('8', Dados.Locais.UF.PR, Dados.Locais.UF.SC)]
+        [InlineData('9', Dados.Locais.UF.RS)]
+        public void CEP_IsValidVarreduraUFPares(char inicioCorreto, params Dados.Locais.UF[] ufs)
+        {
+            var cep = $"{inicioCorreto}0.000-000";
+            
+            foreach (var uf in Enum.GetValues<Dados.Locais.UF>())
+            {
+                // Nãodeve lançar exceções
+                bool contains = ufs.Contains(uf);
+                Assert.Equal(contains, CEP.IsValid(cep, uf));
+            }
+
+        }
+    }
+}
--- Simple.Brazilian/Validadores/CEP.cs
+++ Simple.Brazilian/Validadores/CEP.cs
@@ -4,9 +4,85 @@ namespace Simple.Brazilian.Validadores
 {
     public static class CEP
     {
+        /// <summary>
+        /// Verifica se o CEP tem ocomprimento correto
+        /// </summary>
+        /// <param name="cep">CEP a ser verificado</param>
+        /// <returns>True se o CEP tem o comprimento correto; False senão</returns>
+        public static bool IsValid(string cep)
+        {
+            if (cep == null) return false;
+
+            return Unmask(cep).Length == 8;
+        }
+
+        /// <summary>
+        /// Verifica se o CEP tem ocomprimento correto e se corresponde ao UF designado
+        /// </summary>
+        /// <param name="cep">CEP a ser verificado</param>
+        /// <param name="uf">UF a ser comparado</param>
+        /// <returns>True se o CEP tem o comprimento correto; False senão</returns>
         public static bool IsValid(string cep, Dados.Locais.UF uf)
         {
-            throw new NotImplementedException();
+            // Executa teste base
+            if (!IsValid(cep)) return false;
+
+            // Checa o UF
+            switch (cep[0])
+            {
+                case '0':
+                case '1':
+                    return uf == Dados.Locais.UF.SP;
+
+                case '2':
+                    if (uf == Dados.Locais.UF.RJ) return true;
+                    if (uf == Dados.Locais.UF.ES) return true;
+                    return false;
+
+                case '3':
+                    return uf == Dados.Locais.UF.MG;
+
+                case '4':
+                    if (uf == Dados.Locais.UF.BA) return true;
+                    if (uf == Dados.Locais.UF.SE) return true;
+                    return false;
+
+                case '5':
+                    if (uf == Dados.Locais.UF.PE) return true;
+                    if (uf == Dados.Locais.UF.AL) return true;
+                    if (uf == Dados.Locais.UF.PB) return true;
+                    if (uf == Dados.Locais.UF.RN) return true;
+                    return false;
+
+                case '6':
+                    if (uf == Dados.Locais.UF.CE) return true;
+                    if (uf == Dados.Locais.UF.PI) return true;
+                    if (uf == Dados.Locais.UF.MA) return true;
+                    if (uf == Dados.Locais.UF.PA) return true;
+                    if (uf == Dados.Locais.UF.AP) return true;
+                    if (uf == Dados.Locais.UF.AM) return true;
+                    if (uf == Dados.Locais.UF.RR) return true;
+                    if (uf == Dados.Locais.UF.AC) return true;
+                    return false;
+
+                case '7':
+                    if (uf == Dados.Locais.UF.DF) return true;
+                    if (uf == Dados.Locais.UF.GO) return true;
+                    if (uf == Dados.Locais.UF.TO) return true;
+                    if (uf == Dados.Locais.UF.MT) return true;
+                    if (uf == Dados.Locais.UF.RO) return true;
+                    if (uf == Dados.Locais.UF.MS) return true;
+                    return false;
+
+                case '8':
+                    if (uf == Dados.Locais.UF.PR) return true;
+                    if (uf == Dados.Locais.UF.SC) return true;
+                    return false;
+
+                case '9':
+                    return uf == Dados.Locais.UF.RS;
+            }
+            throw new Exception("Nao é possível comparar o CEP informado com a UF informada");
         }
 
         public static string Mask(string cep) => Formatadores.Texto.AplicaMascara(cep, "__.___-___");
