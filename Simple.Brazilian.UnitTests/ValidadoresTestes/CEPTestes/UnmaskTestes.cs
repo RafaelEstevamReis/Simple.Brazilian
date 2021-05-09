d6ac99d5df8af44075544151f563a0bbc6707ba3
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-05-08 21:56:21 -0300

    Adiciona Unmask para CEP

---
+++ Simple.Brazilian.UnitTests/ValidadoresTestes/CEPTestes/UnmaskTestes.cs
@@ -0,0 +1,16 @@
+﻿using Simple.Brazilian.Validadores;
+using Xunit;
+
+namespace Simple.Brazilian.UnitTests.ValidadoresTestes.CEPTestes
+{
+    public class UnmaskTestes
+    {
+        [Theory]
+        [InlineData("12.345-678", "12345678")]
+        [InlineData("12345-678", "12345678")]
+        public void CEP_Unmask(string entrada, string saida)
+        {
+            Assert.Equal(saida, CEP.Unmask(entrada));
+        }
+    }
+}
