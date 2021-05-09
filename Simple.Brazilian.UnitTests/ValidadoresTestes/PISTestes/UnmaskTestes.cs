a9ca086760bbafdbc10bcf8edcb8dc9b06bdc7c8
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-05-08 21:55:44 -0300

    Adiciona Unmask para PIS

---
+++ Simple.Brazilian.UnitTests/ValidadoresTestes/PISTestes/UnmaskTestes.cs
@@ -0,0 +1,15 @@
+﻿using Simple.Brazilian.Validadores;
+using Xunit;
+
+namespace Simple.Brazilian.UnitTests.ValidadoresTestes.PISTestes
+{
+    public class UnmaskTestes
+    {
+        [Theory]
+        [InlineData("000.00000.00.0", "00000000000")]
+        public void PIS_Unmask(string entrada, string saida)
+        {
+            Assert.Equal(saida, PIS.Unmask(entrada));
+        }
+    }
+}
--- Simple.Brazilian/Validadores/PIS.cs
+++ Simple.Brazilian/Validadores/PIS.cs
@@ -14,7 +14,7 @@ namespace Simple.Brazilian.Validadores
         }
         public static string Unmask(string pis)
         {
-            throw new NotImplementedException();
+            return Formatadores.Texto.RemoveMascara(pis);
         }
     }
 }
