281b254f879dedbea34c806df370b50b93bc5037
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-05-27 21:40:12 -0300

    Adiciona classe para validações 'Mod'

---
+++ Simple.Brazilian/Validators/ModValidation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Simple.Brazilian.Validators
+{
+    public static class ModValidation
+    {
+        public static int[] GenerateReverseSequence(int min, int max, int len)
+        {
+            int[] arr = new int[len];
+            int corrente = min;
+
+            for (int i = len - 1; i >= 0; i--)
+            {
+                arr[i] = corrente;
+                corrente++;
+
+                if (corrente > max) corrente = min;
+            }
+            return arr;
+        }
+        public static int SumMultiply(int[] arr1, int[] arr2)
+        {
+            if (arr1 is null) throw new ArgumentNullException(nameof(arr1));
+            if (arr2 is null) throw new ArgumentNullException(nameof(arr2));
+
+            if (arr1.Length != arr2.Length) throw new ArgumentException("Os comprimentos devem ser iguais");
+
+            if (arr1.Length == 0) return 0;
+
+            int total = 0;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                total += arr1[i] * arr2[i];
+            }
+            return total;
+        }
+        public static int SumMultiply(int[] arr1, char[] arr2)
+        {
+            if (arr1 is null) throw new ArgumentNullException(nameof(arr1));
+            if (arr2 is null) throw new ArgumentNullException(nameof(arr2));
+
+            if (arr1.Length != arr2.Length) throw new ArgumentException("Os comprimentos devem ser iguais");
+
+            if (arr1.Length == 0) return 0;
+
+            int total = 0;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                total += arr1[i] * (arr2[i] - '0');
+            }
+            return total;
+        }
+    }
+}
