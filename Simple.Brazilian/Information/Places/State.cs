f30d2c0ecbc4498b35150b0be959d789121921b3
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-06-27 00:20:00 -0300

    Criação de classe State

---
+++ Simple.Brazilian/Information/Places/State.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Simple.Brazilian.Information.Places
+{
+    public sealed class State
+    {
+        private State() { }
+
+        public string Name { get; set; }
+        public string UF { get; set; }
+        public int IdIBGE { get; set; }
+        public string Capital { get; set; }
+        public double AreaKm2 { get; set; }
+
+
+        internal static State FromIndex(int index)
+        {
+            return new State()
+            {
+                Name = States.Names[index],
+                UF = States.UF[index],
+                IdIBGE = States.IdIBGE[index],
+                Capital = States.Capital[index],
+                AreaKm2 = States.AreaKm2[index],
+            };
+        }
+    }
+}
--- Simple.Brazilian/Information/Places/States.cs
+++ Simple.Brazilian/Information/Places/States.cs
@@ -9,5 +9,11 @@ namespace Simple.Brazilian.Information.Places
         public static readonly int[] IdIBGE = { 12, 27, 13, 16, 29, 23, 53, 32, 52, 21, 31, 50, 51, 15, 25, 26, 22, 41, 33, 24, 11, 14, 43, 42, 28, 35, 17 };
         public static readonly string[] Capital = { "Rio Branco", "Maceió", "Macapá", "Manaus", "Salvador", "Fortaleza", "Brasília", "Vitória", "Goiânia", "São Luís", "Cuiabá", "Campo Grande", "Belo Horizonte", "Belém", "João Pessoa", "Curitiba", "Recife", "Teresina", "Rio de Janeiro", "Natal", "Porto Alegre", "Porto Velho", "Boa Vista", "Florianópolis", "São Paulo", "Aracaju", "Palmas" };
         public static readonly double[] AreaKm2 = { 164_122.2, 27_767.7, 142_814.6, 1_570_745.7, 564_692.7, 148_825.6, 5_822.1, 46_077.5, 340_086.7, 331_983.3, 903_357.9, 357_125.0, 586_528.3, 1_247_689.5, 56_439.8, 199_314.9, 98_311.6, 251_529.2, 43_696.1, 52_796.8, 281_748.5, 237_576.2, 224_299.0, 95_346.2, 248_209.4, 21_910.3, 277_620.9, };
+
+        public State GetStateByUF(string uf) => getByIndex(UF.IndexOf(uf,  StringComparison.CurrentCultureIgnoreCase));
+        public State GetStateByName(string name) => getByIndex(Names.IndexOf(name, StringComparison.CurrentCultureIgnoreCase));
+        public State GetStateByName(int id) => getByIndex(IdIBGE.IndexOf(id));
+
+        private State getByIndex(int index) => State.FromIndex(index);
     }
 }
