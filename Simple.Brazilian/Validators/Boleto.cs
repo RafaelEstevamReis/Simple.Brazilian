dd8f0f5a9f60289db978243234cbac54453d12a5
Mateus Moraes <mtu_h@hotmail.com>
2021-07-15 18:01:41 -0300

    criando a parte de boletos

---
+++ Simple.Brazilian/Validators/Boleto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple.Brazilian.Validators
+{
+    public static class Boleto
+    {
+        public static int[] CalculateMod10(string LinhaDigitavelBoleto)
+        {
+            int[] camposDV = new int[3];
+            int soma = 0;
+
+            for (int i = 0; i < 29; i++)
+            {
+                int digito = Convert.ToInt32(LinhaDigitavelBoleto[i..(i + 1)]);
+                int peso = i % 2 == 0 ? 2 : 1;
+
+                int valor = digito * peso;
+
+                if (valor < 10)
+                {
+                    soma += valor;
+                }
+                else
+                {
+                    int d1 = valor % 10;
+                    int d2 = valor / 10;
+
+                    soma += d1;
+                    soma += d2;
+                }
+
+                if (i == 8) camposDV[0] = 30 - (soma % 10);
+                if (i == 18) camposDV[1] = 40 - ((soma - camposDV[0]) % 10);
+                if (i == 28) camposDV[2] = 40 - ((soma - (camposDV[0] + camposDV[1])) % 10);
+            }
+            return camposDV;
+        }
+
+
+    }
+}
