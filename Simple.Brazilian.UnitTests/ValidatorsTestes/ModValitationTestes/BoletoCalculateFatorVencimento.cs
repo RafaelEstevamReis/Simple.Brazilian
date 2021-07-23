e68a78faed5c000d0401cdc4520a975c8549243c
Mateus Moraes <mtu_h@hotmail.com>
2021-07-23 14:38:54 -0300

    Escrevendo os testes

---
+++ Simple.Brazilian.UnitTests/ValidatorsTestes/ModValitationTestes/BoletoCalculateFatorVencimento.cs
@@ -0,0 +1,29 @@
+﻿using Simple.Brazilian.Validators;
+using Xunit;
+
+namespace Simple.Brazilian.UnitTests.ValidatorsTestes.ModValitationTestes
+{
+    public class BoletoCalculateFatorVencimento
+    {
+        [Theory]
+        [InlineData("20000703", 1000)]
+        [InlineData("20021201", 1881)]
+        [InlineData("20070329", 3460)]
+        [InlineData("20250221", 9999)]
+        public void CalculaFatorVencimento_Formatado(string data, int inum)
+        {
+            Assert.Equal(inum, Boleto.FatorVencimento(data));
+        }
+
+        [Theory]
+        [InlineData("03/07/2000", 1000)]
+        [InlineData("01/12/2002", 1881)]
+        [InlineData("29/03/2007", 3460)]
+        [InlineData("21/02/2025", 9999)]
+
+        public void CalculaFatorVencimento_Raw(string data, int inum)
+        {
+            Assert.Equal(inum, Boleto.FatorVencimento(data));
+        }
+    }
+}
