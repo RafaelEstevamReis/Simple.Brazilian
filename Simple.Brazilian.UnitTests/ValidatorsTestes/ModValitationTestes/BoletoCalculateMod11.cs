4bb45d85054c5ea41d948cb5a8eb8f45ecc15d34
Mateus Moraes <mtu_h@hotmail.com>
2021-07-23 10:19:19 -0300

    Contrução do teste mod11

--- Simple.Brazilian.UnitTests/ValidatorsTestes/ModValitationTestes/BoletoCalculateMod11.cs
+++ Simple.Brazilian.UnitTests/ValidatorsTestes/ModValitationTestes/BoletoCalculateMod11.cs
@@ -6,8 +6,14 @@ namespace Simple.Brazilian.UnitTests.ValidatorsTestes.ModValitationTestes
     public class BoletoCalculateMod11
     {
         [Theory]
+        // BB: Boleto Banco do Brasil
         [InlineData("0019050095", "40144816069", "06809350314", 3)]
-        //[InlineData("3419157569", "92889300668", "60633340009", 5)]
+        // Itau: Boleto ex "como identificar boletos falsos.pdf"
+        [InlineData("3419109016", "04197934567", "25530670006", 1)]
+        // Itau: Boleto real do cliente Itau
+        [InlineData("3419157569", "92889300668", "60633340009", 5)]
+        // Itau: Boleto ex Itau
+        [InlineData("3419110121", "34567880058", "71234570001", 6)]
 
         public void CalculateMod11(string campo1, string campo2, string campo3, int inum)
         {
