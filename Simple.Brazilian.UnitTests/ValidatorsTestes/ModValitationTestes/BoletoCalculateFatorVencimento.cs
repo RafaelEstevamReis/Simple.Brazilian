da14fdd8897849231266feb0f01d06b58b16853c
Mateus Moraes <mtu_h@hotmail.com>
2021-07-27 18:05:29 -0300

    Feito o teste de validação do Fator de Vencimento

--- Simple.Brazilian.UnitTests/ValidatorsTestes/ModValitationTestes/BoletoCalculateFatorVencimento.cs
+++ Simple.Brazilian.UnitTests/ValidatorsTestes/ModValitationTestes/BoletoCalculateFatorVencimento.cs
@@ -6,16 +6,6 @@ namespace Simple.Brazilian.UnitTests.ValidatorsTestes.ModValitationTestes
     public class BoletoCalculateFatorVencimento
     {
         [Theory]
-        [InlineData("20000703", 1000)]
-        [InlineData("20021201", 1881)]
-        [InlineData("20070329", 3460)]
-        [InlineData("20250221", 9999)]
-        public void CalculaFatorVencimento_Formatado(string data, int inum)
-        {
-            Assert.Equal(inum, Boleto.FatorVencimento(data));
-        }
-
-        [Theory]
         [InlineData("03/07/2000", 1000)]
         [InlineData("01/12/2002", 1881)]
         [InlineData("29/03/2007", 3460)]
