73504393ff8635aa02d3047828abed6e475f1288
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-08-10 09:59:43 -0300

    Adiciona testes de erro

--- Simple.Brazilian.UnitTests/ValidatorsTestes/BoletoTestes/BoletoCalculateFatorVencimento.cs
+++ Simple.Brazilian.UnitTests/ValidatorsTestes/BoletoTestes/BoletoCalculateFatorVencimento.cs
@@ -16,5 +16,23 @@ namespace Simple.Brazilian.UnitTests.ValidatorsTestes.BoletoTestes
         {
             Assert.Equal(valorDias, Boleto.CalculateDueDateFactor(data));
         }
+
+        [Theory]
+        [InlineData("06/10/1997", -1)]
+        [InlineData("22/02/2025", 10_000)]
+        public void CalculaFatorVencimento_InvalidosNumérico(string data, int valorDias)
+        {
+            Assert.Equal(valorDias, Boleto.CalculateDueDateFactor(data));
+        }
+
+        [Theory]
+        [InlineData("43/07/2000")] // dia inválido
+        [InlineData("01/13/2002")] // mês inválido (formato US)
+        [InlineData("29/03/07")] // ano abreviado
+        [InlineData("2020/01/01")] // formato INT
+        public void CalculaFatorVencimento_InvalidosParse(string data)
+        {
+            Assert.Throws<FormatException>(() => Boleto.CalculateDueDateFactor(data));
+        }
     }
 }
