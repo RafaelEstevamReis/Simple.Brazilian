4f445e46fccc78975d39fe0c3bf7d5061b85edc2
DaniloOtavio <danilosilva.tecnico@gmail.com>
2021-11-12 17:16:12 -0300

    IsNullOrWhiteSpace para NET20

---
+++ Simple.Brazilian/Compatibilidade/Net20_IsNullOrWhiteSpace.cs
@@ -0,0 +1,27 @@
+﻿#if NET20
+namespace System.StringNET20
+{
+    public static class StringNET20
+    {
+        public static bool IsNullOrWhiteSpace(string s)
+        {
+            bool null_ws = false;
+
+            if (s is null) null_ws = true;
+            if (s.Length == 0) null_ws = true;
+            if (s.Trim() == "") null_ws = true;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == ' ' || s[i] == 10 || s[i] == 10) null_ws = true;
+                else
+                {
+                    null_ws = false;
+                    break;
+                }
+            }
+            return null_ws;
+        }
+    }
+}
+#endif
