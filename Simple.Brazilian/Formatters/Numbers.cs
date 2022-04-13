10f743756ed3ad48f07e49a0d48e60a7fff46841
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-04-12 21:24:03 -0300

    Adiciona documentação no Formatters.Numbers

--- Simple.Brazilian/Formatters/Numbers.cs
+++ Simple.Brazilian/Formatters/Numbers.cs
@@ -3,8 +3,14 @@ using System.Globalization;
 
 namespace Simple.Brazilian.Formatters
 {
+    /// <summary>
+    /// Formata números
+    /// </summary>
     public class Numbers
     {
+        /// <summary>
+        /// CultureInfo padrão a ser utilizado
+        /// </summary>
         public static CultureInfo DefaultCulture { get; }
         static Numbers()
         {
@@ -14,7 +20,13 @@ namespace Simple.Brazilian.Formatters
             DefaultCulture = new CultureInfo("pt-BR", false);
 #endif
         }
-
+        
+        /// <summary>
+        /// Parseia um texto para Int32
+        /// </summary>
+        /// <param name="text">Texto a ser convertido</param>
+        /// <param name="OnError">Valor a ser retornado em caso de falha</param>
+        /// <returns>Número convertido, ou valor padrão</returns>
         public static int ToInt(string text, int? OnError = 0)
         {
             if (int.TryParse(text, NumberStyles.Any, DefaultCulture, out int value)) return value;
@@ -28,21 +40,43 @@ namespace Simple.Brazilian.Formatters
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// Converte número em texto usando DefaultCulture
+        /// </summary>
+        /// <param name="value">Valor a ser convertido</param>
+        /// <returns>Texto convertido</returns>
         public static string ToText(int value)
         {
             return Convert.ToString(value, DefaultCulture);
         }
+        /// <summary>
+        /// Converte número em texto usando DefaultCulture com a quantidade definida de casas decimais
+        /// </summary>
+        /// <param name="value">Valor a ser convertido</param>
+        /// <param name="decimals">Casas decimais</param>
+        /// <returns>Texto convertido</returns>
         public static string ToText(double value, int decimals = 2)
         {
             var rounded = abntRounding(value, decimals);
             return rounded.ToString($"N{decimals}", DefaultCulture);
         }
+        /// <summary>
+        /// Converte número em texto usando DefaultCulture com a quantidade definida de casas decimais
+        /// </summary>
+        /// <param name="value">Valor a ser convertido</param>
+        /// <param name="decimals">Casas decimais</param>
+        /// <returns>Texto convertido</returns>
         public static string ToText(decimal value, int decimals = 2)
         {
             var rounded = abntRounding(value, decimals);
             return rounded.ToString($"N{decimals}", DefaultCulture);
         }
+        /// <summary>
+        /// Converte número em texto monetário usando DefaultCulture com a quantidade definida de casas decimais
+        /// </summary>
+        /// <param name="value">Valor a ser convertido</param>
+        /// <param name="decimals">Casas decimais</param>
+        /// <returns>Texto convertido</returns>
         public static string ToBRL(decimal value, int decimals = 2)
         {
             var rounded = abntRounding(value, decimals);
