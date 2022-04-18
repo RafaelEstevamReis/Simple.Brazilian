a70d8d94e3042c7a286543094cf2f1c7cd2072f7
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-04-18 20:04:51 -0300

    Add WorkDay

---
+++ Simple.Brazilian.UnitTests/InformationTestes/DatesTestes/WorkDayTestes.cs
@@ -0,0 +1,54 @@
+﻿using Simple.Brazilian.Information;
+using System;
+using Xunit;
+
+namespace Simple.Brazilian.UnitTests.InformationTestes.DatesTestes
+{
+    public class WorkDayTestes
+    {
+        [Theory]
+        [InlineData(1, WorkDay.DayType.NatinalHoliday)]
+        [InlineData(2, WorkDay.DayType.WeekEnd)]
+        [InlineData(3, WorkDay.DayType.WeekEnd)]
+        [InlineData(4, WorkDay.DayType.WeekDay)]
+        public void GetDayTypeTestes_May2020(int day, WorkDay.DayType type)
+        {
+            var date = new DateTime(2020, 5, day);
+            Assert.Equal(type, WorkDay.GetDayType(date));
+        }
+
+        [Fact]
+        public void WorkDaysBetween_Exemplo_Jan2021()
+        {
+            DateTime dtInicio = new DateTime(2021, 01, 01);
+            DateTime dtFim = new DateTime(2021, 01, 31);
+
+            Assert.Equal(20, WorkDay.WorkDaysBetween(dtInicio, dtFim));
+        }
+        [Fact]
+        public void WorkDaysBetween_Exemplo_Jan2020()
+        {
+            DateTime dtInicio = new DateTime(2020, 1, 01);
+            DateTime dtFim = new DateTime(2020, 1, 31);
+
+            Assert.Equal(22, WorkDay.WorkDaysBetween(dtInicio, dtFim));
+        }
+        [Fact]
+        public void WorkDaysBetween_Exemplo_May2020()
+        {
+            DateTime dtInicio = new DateTime(2020, 05, 01);
+            DateTime dtFim = new DateTime(2020, 05, 31);
+
+            Assert.Equal(20, WorkDay.WorkDaysBetween(dtInicio, dtFim));
+        }
+        [Fact]
+        public void WorkDaysBetween_Exemplo_2020()
+        {
+            DateTime dtInicio = new DateTime(2020, 01, 01);
+            DateTime dtFim = new DateTime(2020, 12, 31);
+
+            Assert.Equal(255, WorkDay.WorkDaysBetween(dtInicio, dtFim));
+        }
+
+    }
+}
---
+++ Simple.Brazilian/Information/WorkDay.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Simple.Brazilian.Information
+{
+    /// <summary>
+    /// Classe para lidar com dias úteis
+    /// Segundo as leis 10.607/2002 e 6.802/1980
+    /// </summary>
+    public class WorkDay
+    {
+        /// <summary>
+        /// Tipo de data
+        /// </summary>
+        public enum DayType
+        {
+            /// <summary>
+            /// Feriado nacional, ver Dates.IsNationalHoliday
+            /// </summary>
+            NatinalHoliday,
+            /// <summary>
+            /// Fim de semana
+            /// </summary>
+            WeekEnd,
+            /// <summary>
+            /// Dia de semana
+            /// </summary>
+            WeekDay,
+        }
+        /// <summary>
+        /// Obtém o tipo de dia
+        /// </summary>
+        /// <param name="date">Data base</param>
+        /// <returns>Tipo de data</returns>
+        public static DayType GetDayType(DateTime date)
+        {
+            if (Dates.IsNationalHoliday(date, out _)) return DayType.NatinalHoliday;
+
+            if (date.DayOfWeek == DayOfWeek.Saturday) return DayType.WeekEnd;
+            if (date.DayOfWeek == DayOfWeek.Sunday) return DayType.WeekEnd;
+
+            return DayType.WeekDay;
+        }
+        /// <summary>
+        /// Obtém dias úteis entre as datas
+        /// </summary>
+        /// <param name="start">Data de início, inclusive</param>
+        /// <param name="end">Data de dim, inclusive</param>
+        /// <returns>Dias úteis</returns>
+        public static int WorkDaysBetween(DateTime start, DateTime end)
+        {
+            int count = 0;
+            for (DateTime dt = start; dt <= end; dt = dt.AddDays(1))
+            {
+                var type = GetDayType(dt);
+                if(type == DayType.WeekDay) count++;
+            }
+            return count;
+        }
+
+    }
+}
