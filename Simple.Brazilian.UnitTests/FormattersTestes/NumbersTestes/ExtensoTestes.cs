a964dc917bcc6a2611004ebfc449cb7b47f8df71
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-07-15 01:27:22 -0300

    Números por Extenso

---
+++ Simple.Brazilian.UnitTests/FormattersTestes/NumbersTestes/ExtensoTestes.cs
@@ -0,0 +1,97 @@
+﻿using Simple.Brazilian.Formatters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Simple.Brazilian.UnitTests.FormattersTestes.NumbersTestes
+{
+    public class ExtensoTestes
+    {
+        [Fact]
+        public void Numero_Extenso_ULongMax()
+        {
+            // 18.446.744.073.709.551.615
+            Assert.Equal("Dezoito Quintilhões Quatrocentos e Quarenta e Seis Quatrilhões Setecentos e Quarenta e Quatro Trilhões Setenta e Três Bilhões Setecentos e Nove Milhões Quinhentos e Cinquenta e Um Mil Seiscentos e Quinze", 
+                         Numbers.NumeroExtenso(ulong.MaxValue));
+        }
+        [Fact]
+        public void Numero_Extenso_UIntMax()
+        {
+            // 4.294.967.295
+            Assert.Equal("Quatro Bilhões Duzentos e Noventa e Quatro Milhões Novecentos e Sessenta e Sete Mil Duzentos e Noventa e Cinco",
+                         Numbers.NumeroExtenso(uint.MaxValue));
+        }
+
+        [Theory]
+        [InlineData(0, "Zero")]
+        [InlineData(1_000, "Mil")]
+        // https://duvidas.dicio.com.br/como-escrever-numeros-por-extenso/
+        // [1] Deverá ser usada a conjunção e entre as centenas, dezenas e unidades
+        [InlineData(37, "Trinta e Sete")]
+        [InlineData(237, "Duzentos e Trinta e Sete")]
+        [InlineData(207, "Duzentos e Sete")]
+        // [2] Na separação dos milhares e das centenas, a conjunção e apenas deverá ser usada quando o número termina nas centenas
+        [InlineData(5_500, "Cinco Mil e Quinhentos")]
+        [InlineData(1_300, "Mil e Trezentos")]
+        [InlineData(8_100, "Oito Mil e Cem")]
+        // [3] Quando o número começa nos milhares e termina nas dezenas ou nas unidades, não deverá ser usada a conjunção e na separação das centenas,
+        // apenas na separação das restantes ordens
+        [InlineData(2_125, "Dois Mil Cento e Vinte e Cinco")]
+        [InlineData(7_491, "Sete Mil Quatrocentos e Noventa e Um")]
+        [InlineData(6_510, "Seis Mil Quinhentos e Dez")]
+        [InlineData(9_030, "Nove Mil e Trinta")]
+        [InlineData(9_003, "Nove Mil e Três")]
+        // [4] Quando o número é muito grande, a leitura é feita por classes.
+        // A conjunção e é usada para separar os algarismos da classe mas não deverá ser usada para separar as classes
+        [InlineData(378_027_312L, "Trezentos e Setenta e Oito Milhões Vinte e Sete Mil Trezentos e Doze")]
+        [InlineData(209_670_123_418L, "Duzentos e Nove Bilhões Seiscentos e Setenta Milhões Cento e Vinte e Três Mil Quatrocentos e Dezoito")]
+        // Outros casos
+        [InlineData(1_000_090_000_003, "Um Trilhão Noventa Milhões e Três")]
+        [InlineData(2_000_090_000_030, "Dois Trilhões Noventa Milhões e Trinta")]
+        [InlineData(1_000_090_000_500, "Um Trilhão Noventa Milhões e Quinhentos")]
+        public void Numero_Extenso(ulong numero, string correto)
+        {
+            var calculado = Numbers.NumeroExtenso(numero);
+            Assert.Equal(correto, calculado);
+        }
+
+
+        [Fact]
+        public void Numero_ValorExtenso_UIntMax()
+        {
+            // 4.294.967.295
+            Assert.Equal("Quatro Bilhões Duzentos e Noventa e Quatro Milhões Novecentos e Sessenta e Sete Mil Duzentos e Noventa e Cinco Reais",
+                         Numbers.MoedaExtenso(uint.MaxValue));
+        }
+
+        [Theory]
+        [InlineData(1, "Um Real")]
+        [InlineData(1.01, "Um Real e Um Centavo")]
+        [InlineData(1_000_090_000_003, "Um Trilhão Noventa Milhões e Três Reais")]
+        [InlineData(2_000_090_000_030, "Dois Trilhões Noventa Milhões e Trinta Reais")]
+        [InlineData(1_000_090_000_500, "Um Trilhão Noventa Milhões e Quinhentos Reais")]
+        [InlineData(1_000_090_000_003.01, "Um Trilhão Noventa Milhões e Três Reais e Um Centavo")]
+        [InlineData(2_000_090_000_030.02, "Dois Trilhões Noventa Milhões e Trinta Reais e Dois Centavos")]
+        [InlineData(2_000_090_000_000.02, "Dois Trilhões Noventa Milhões de Reais e Dois Centavos")]
+        // https://www.todamateria.com.br/numeros-por-extenso/
+        [InlineData(0.01, "Um Centavo de Real")]
+        [InlineData(0.02, "Dois Centavos de Real")]
+        [InlineData(1.03, "Um Real e Três Centavos")]
+        [InlineData(2.00, "Dois Reais")]
+        [InlineData(10.0, "Dez Reais")]
+        [InlineData(100, "Cem Reais")]
+        [InlineData(1_000, "Mil Reais")]
+        [InlineData(1_000_000, "Um Milhão de Reais")]
+        [InlineData(2_000_000_000, "Dois Bilhões de Reais")]
+        public void Numero_MoedaExtenso(decimal numero, string correto)
+        {
+            var calculado = Numbers.MoedaExtenso(numero);
+            Assert.Equal(correto, calculado);
+        }
+
+
+    }
+}
--- Simple.Brazilian/Formatters/Numbers.cs
+++ Simple.Brazilian/Formatters/Numbers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
 
@@ -13,6 +14,28 @@ namespace Simple.Brazilian.Formatters
         /// CultureInfo padrão a ser utilizado
         /// </summary>
         public static CultureInfo DefaultCulture { get; }
+        private static readonly string[] tamanhoNumeroPlural = new string[] {
+            "",
+            "Mil",
+            "Milhões",
+            "Bilhões",
+            "Trilhões",
+            "Quatrilhões",
+            "Quintilhões", // limite do ULONG
+            "Sextilhões",
+            "Septilhões",
+        };
+        private static readonly string[] tamanhoNumeroSingular = new string[] {
+            "",
+            "Mil",
+            "Milhão",
+            "Bilhão",
+            "Trilhão",
+            "Quatrilhão",
+            "Quintilhão", // limite do ULONG
+            "Sextilhão",
+            "Septilhão",
+        };
         static Numbers()
         {
 #if NETSTANDARD1_0
@@ -176,5 +199,197 @@ namespace Simple.Brazilian.Formatters
             if (value.HasValue) return value.Value;
             throw new FormatException();
         }
+
+        /// <summary>
+        /// Escreve número monetário por extenso
+        /// </summary>
+        /// <exception cref="NotSupportedException">Valores negativos não são suportados</exception>
+        public static string MoedaExtenso(decimal valorReais)
+        {
+            if (valorReais == 0) return "Zero Reais";
+            if (valorReais < 0) throw new NotSupportedException("Valores negativos não são suportados");
+
+            ulong inteira = Convert.ToUInt64(valorReais);
+            uint centavos = Convert.ToUInt32(valorReais * 100 % 100);
+
+            StringBuilder sbTexto = new StringBuilder();
+            if (inteira > 0)
+            {
+                var ext = NumeroExtenso(inteira);
+                sbTexto.Append(ext);
+                if (inteira == 1) sbTexto.Append(" Real");
+                else
+                {
+                    // termina com "hão" ou "hões"
+                    if(ext.EndsWith("hão") || ext.EndsWith("hões"))
+                    {
+                        sbTexto.Append(" de");
+                    }
+                    sbTexto.Append(" Reais");
+                }
+
+                if (centavos > 0) sbTexto.Append(" e ");
+            }
+            if(centavos > 0)
+            {
+                sbTexto.Append(NumeroExtenso(centavos));
+                if(centavos == 1) sbTexto.Append(" Centavo");
+                else sbTexto.Append(" Centavos");
+
+                if(inteira == 0) sbTexto.Append(" de Real");
+            }
+
+            return sbTexto.ToString();
+        }
+
+        /// <summary>
+        /// Escreve número inteiro por extenso
+        /// </summary>
+        public static string NumeroExtenso(ulong numero)
+        {
+            if (numero == 0) return "Zero";
+
+            // Quebra em blocos de 3 dígitos
+            List<int> partes = new List<int>();
+            while (numero > 0)
+            {
+                partes.Add((int)(numero % 1_000));
+                numero /= 1_000;
+            }
+            // Processa os blocos
+            StringBuilder sbTexto = new StringBuilder();
+            for (int i = partes.Count - 1; i >= 0; i--)
+            {
+                // Se não tem bloco, não coloca
+                if (partes[i] == 0)
+                {
+                    // exceto no final, caso não tenha milhar mas tenha Centena, Dezena ou Unidade
+                    if (i == 1 && partes[0] > 0) sbTexto.Append("e ");
+
+                    continue;
+                }
+
+                if (i == 1 && partes[1] == 1)
+                {
+                    // Não fazer "Um Mil"
+                }
+                else
+                {
+                    extensoParte3(partes[i], sbTexto);
+                }
+                // Adiciona separação dos blocos
+                if (i > 0)
+                {
+                    sbTexto.Append(" ");
+
+                    if (partes[i] == 1) sbTexto.Append(tamanhoNumeroSingular[i]);
+                    else sbTexto.Append(tamanhoNumeroPlural[i]);
+
+                    // Na separação dos milhares e das centenas, a conjunção e apenas deverá ser usada quando o número termina nas centenas:
+                    if (i == 1 && partes[0] % 100 == 0 && partes[0] != 0)
+                    {
+                        sbTexto.Append(" e ");
+                    }
+                    else if (i == 1 && partes[0] / 100 == 0 && partes[0] > 0)
+                    {
+                        sbTexto.Append(" e ");
+                    }
+                    else
+                    {
+                        // Quando o número é muito grande, a leitura é feita por classes.
+                        // A conjunção e é usada para separar os algarismos da classe mas não deverá ser usada para separar as classes.
+                        // Não deverá ser usada nem a conjunção e nem a vírgula
+                        sbTexto.Append(" ");
+                    }
+                }
+            }
+            return sbTexto.ToString().Trim();
+        }
+
+        private static void extensoParte3(int numero, StringBuilder sbTexto)
+        {
+            int centena = numero / 100;
+            int dezena = numero / 10 % 10;
+            int unidade = numero % 10;
+            bool adicionarE = false;
+
+            if (centena == 1 && (dezena > 0 || unidade > 0))
+            {
+                sbTexto.Append("Cento");
+                adicionarE = true;
+            }
+            else if (centena > 0)
+            {
+                sbTexto.Append(numerosFixos(centena * 100));
+                adicionarE = true;
+            }
+
+            if (dezena == 1)
+            {
+                if (adicionarE) sbTexto.Append(" e ");
+
+                sbTexto.Append(numerosFixos(dezena * 10 + unidade));
+                return;
+            }
+
+            if (dezena > 0)
+            {
+                if (adicionarE)
+                {
+                    sbTexto.Append(" e ");
+                }
+
+                sbTexto.Append(numerosFixos(dezena * 10));
+                adicionarE = true;
+            }
+
+            if (unidade > 0)
+            {
+                if (adicionarE) sbTexto.Append(" e ");
+                sbTexto.Append(numerosFixos(unidade));
+            }
+        }
+
+        private static string numerosFixos(int numeroBase)
+            => numeroBase switch
+            {
+                1 => "Um",
+                2 => "Dois",
+                3 => "Três",
+                4 => "Quatro",
+                5 => "Cinco",
+                6 => "Seis",
+                7 => "Sete",
+                8 => "Oito",
+                9 => "Nove",
+                10 => "Dez",
+                11 => "Onze",
+                12 => "Doze",
+                13 => "Treze",
+                14 => "Quatorze",
+                15 => "Quinze",
+                16 => "Dezesseis",
+                17 => "Dezessete",
+                18 => "Dezoito",
+                19 => "Dezenove",
+                20 => "Vinte",
+                30 => "Trinta",
+                40 => "Quarenta",
+                50 => "Cinquenta",
+                60 => "Sessenta",
+                70 => "Setenta",
+                80 => "Oitenta",
+                90 => "Noventa",
+                100 => "Cem",
+                200 => "Duzentos",
+                300 => "Trezentos",
+                400 => "Quatrocentos",
+                500 => "Quinhentos",
+                600 => "Seiscentos",
+                700 => "Setecentos",
+                800 => "Oitocentos",
+                900 => "Novecentos",
+                _ => throw new ArgumentException("Número inválido"),
+            };
     }
 }
