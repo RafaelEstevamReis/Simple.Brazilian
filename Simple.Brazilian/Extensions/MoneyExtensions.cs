87905fcafe0d028902a219094d85427980b2df9b
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-09-24 18:06:33 -0300

    Add Money Extension

---
+++ Simple.Brazilian/Extensions/MoneyExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Simple.Brazilian
+{
+    public static class MoneyExtensions
+    {
+        /// <summary>
+        /// Converte número em texto monetário usando Formatters.Numbers.ToBRL com duas casas decimais
+        /// </summary>
+        /// <param name="value">Valor a ser convertido</param>
+        /// <returns>Texto convertido</returns>
+        public static string ToBRL(decimal value)
+            => Formatters.Numbers.ToBRL(value);
+
+    }
+}
