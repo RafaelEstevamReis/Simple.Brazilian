c27a1fa7b6244adfe20efeeae283dd2263afa0aa
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-09-24 18:08:51 -0300

    Update MoneyExtensions.cs

--- Simple.Brazilian/Extensions/MoneyExtensions.cs
+++ Simple.Brazilian/Extensions/MoneyExtensions.cs
@@ -1,14 +1,18 @@
 ﻿namespace Simple.Brazilian
 {
+    /// <summary>
+    /// Extensões para valores monetários
+    /// </summary>
     public static class MoneyExtensions
     {
         /// <summary>
-        /// Converte número em texto monetário usando Formatters.Numbers.ToBRL com duas casas decimais
+        /// Converte número em texto monetário usando Formatters.Numbers.ToBRL com casas decimais especificadas
         /// </summary>
         /// <param name="value">Valor a ser convertido</param>
+        /// <param name="decimals">Casas decimais</param>
         /// <returns>Texto convertido</returns>
-        public static string ToBRL(decimal value)
-            => Formatters.Numbers.ToBRL(value);
+        public static string ToBRL(decimal value, int decimals = 2)
+            => Formatters.Numbers.ToBRL(value, decimals);
 
     }
 }
