2e0079c2e15e6abbfd7bc0c23a74ea7db36b3b9f
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-10-25 18:48:12 -0300

    Add CRC16

---
+++ Simple.Brazilian.UnitTests/ValidatorsTestes/CRC/CRCTestes.cs
@@ -0,0 +1,30 @@
+﻿using Simple.Brazilian.Validators;
+using Xunit;
+
+namespace Simple.Brazilian.UnitTests.ValidatorsTestes.CRC
+{
+    public class CRCTestes
+    {
+        [Fact]
+        public void CRC16_Bytes1()
+        {
+            var bytes = new byte[] { 1, 2, 3, 4 };
+            int result = 35267;
+            Assert.Equal(result, CRC16.ComputeCRC(bytes));
+        }
+        [Fact]
+        public void CRC16_Bytes2()
+        {
+            var bytes = new byte[] { 0x71, 0x82, 0x93, 0xA4 };
+            int result = 24045;
+            Assert.Equal(result, CRC16.ComputeCRC(bytes));
+        }
+        [Fact]
+        public void CRC16_Text()
+        {
+            string text = "Simple.Brazilian";
+            var result = "12EE";
+            Assert.Equal(result, CRC16.ComputeHexCRC(text));
+        }
+    }
+}
