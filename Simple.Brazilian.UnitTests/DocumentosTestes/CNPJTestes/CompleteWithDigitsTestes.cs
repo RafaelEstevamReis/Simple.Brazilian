9735d8a0dc22e7c764d4e1c4630fc904489f1395
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-10-28 12:23:34 -0300

    Adiciona CNPJ.CompleteWithDigitsCNPJ

---
+++ Simple.Brazilian.UnitTests/DocumentosTestes/CNPJTestes/CompleteWithDigitsTestes.cs
@@ -0,0 +1,25 @@
+﻿using Simple.Brazilian.Documents;
+using Xunit;
+
+namespace Simple.Brazilian.UnitTests.DocumentosTestes.CNPJTestes
+{
+    public class CompleteWithDigitsTestes
+    {
+        [Theory]
+        [InlineData("16553608000183")]
+        [InlineData("67406732000157")]
+        [InlineData("02361553000103")] // primeiro dígito == 10
+        [InlineData("34534876000130")] // Segundo dígito == 10
+        [InlineData("37238174000105")]
+        [InlineData("39365864000142")]
+        [InlineData("34742750000150")]
+        [InlineData("17189722000139")] // SHARP Sistemas
+        public void CNPJ_CompleteWithDigitsCNPJ(string cnpjCompleto)
+        {
+            string cnpjParcial = cnpjCompleto[..12];
+            var result = CNPJ.CompleteWithDigitsCNPJ(cnpjParcial);
+
+            Assert.Equal(cnpjCompleto, result);
+        }
+    }
+}
--- Simple.Brazilian/Documents/CNPJ.cs
+++ Simple.Brazilian/Documents/CNPJ.cs
@@ -1,4 +1,6 @@
-﻿namespace Simple.Brazilian.Documents
+﻿using System;
+
+namespace Simple.Brazilian.Documents
 {
     /// <summary>
     /// Validador de CNPJ
@@ -35,15 +37,41 @@
             int firstDigit = cnpj[12] - '0';
             int secondDigit = cnpj[13] - '0';
 
+            if (!CalculateDigits(cnpj, out int firstDigitVerification, out int secondDigitVerification)) 
+                return false;
+
+            if (firstDigitVerification != firstDigit || secondDigitVerification != secondDigit)
+                return false;
+
+            // Remove "11111..."
+            // Não é válido se qualquer um dos dígitos for diferente
+            for (int i = 1; i < 14; i++)
+            {
+                // Se algum for diferente, o CNPJ é válido
+                if (cnpj[i] != cnpj[0]) return true;
+            }
+
+            return false;
+        }
+
+        private static bool CalculateDigits(string cnpj, out int firstDigitVerification, out int secondDigitVerification)
+        {
             int firstSum = 0;
             int secondSum = 0;
             int countdown1 = 5;
             int countdown2 = 6;
+            // Calcula simultâneo
             for (int i = 0; i < 12; i++)
             {
                 int intValue = cnpj[i] - '0';
                 // Não é número ?
-                if (intValue > 9 || intValue < 0) return false;
+                if (intValue > 9 || intValue < 0)
+                {
+                    firstDigitVerification = 0;
+                    secondDigitVerification = 0;
+
+                    return false; // Interrompe
+                }
 
                 firstSum += intValue * countdown1;
                 secondSum += intValue * countdown2;
@@ -54,28 +82,41 @@
                 if (countdown1 == 1) countdown1 = 9;
                 if (countdown2 == 1) countdown2 = 9;
             }
-            secondSum += 2 * firstDigit;
-
-            int firstDigitVerification = (firstSum * 10) % 11;
-            int secondDigitVerification = (secondSum * 10) % 11;
-
+            
+            // Termina o dígito 1
+            firstDigitVerification = (firstSum * 10) % 11;
             if (firstDigitVerification == 10)
                 firstDigitVerification = 0;
+            
+            // Termina o dígito 2
+            secondSum += 2 * firstDigitVerification;
+            secondDigitVerification = (secondSum * 10) % 11;
+
             if (secondDigitVerification == 10)
                 secondDigitVerification = 0;
 
-            if (firstDigitVerification != firstDigit || secondDigitVerification != secondDigit)
-                return false;
-
-            // Não é válido se qualquer um dos dígitos for diferente
-            for (int i = 1; i < 14; i++)
-            {
-                // Se algum for diferente, o CPF é válido
-                if (cnpj[i] != cnpj[0]) return true;
-            }
-
-            return false;
+            // Finalizou
+            return true;
         }
+
+        /// <summary>
+        /// Completa um CNPJ que esteja sem os dígitos verificadores
+        /// </summary>
+        /// <param name="partialCNPJ">CNPJ parcial com 12 dígitos a ser completado</param>
+        /// <returns>CPF válido</returns>
+        /// <exception cref="ArgumentNullException">Parâmetro não deve ser NULL</exception>
+        /// <exception cref="ArgumentException">Parâmetro informado é inválido</exception>
+        public static string CompleteWithDigitsCNPJ(string partialCNPJ)
+        {
+            if (partialCNPJ is null) throw new ArgumentNullException(nameof(partialCNPJ));
+            if (partialCNPJ.Length != 12) throw new ArgumentException($"{nameof(partialCNPJ)} deve ser composto pelos 12 dígitos iniciais");
+
+            bool valid = CalculateDigits(partialCNPJ, out int d1, out int d2);
+            if (!valid) throw new ArgumentException($"{nameof(partialCNPJ)} é inválido");
+
+            return $"{partialCNPJ}{d1}{d2}";
+        }
+
 
         /// <summary>
         /// Aplica a máscara de CNPJ __.___.___/____-__
