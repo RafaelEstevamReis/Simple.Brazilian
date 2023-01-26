f2cc8bfaff8c2e886f6b48e8c6586c2d878b2c6b
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-26 10:36:30 -0300

    Adiciona validação de e-mail

---
+++ Simple.Brazilian.UnitTests/ValidatorsTestes/EMailTestes/IsValidTests.cs
@@ -0,0 +1,50 @@
+﻿using Xunit;
+
+namespace Simple.Brazilian.UnitTests.ValidatorsTestes.EMailTestes
+{
+    public class IsValidTests
+    {
+        [Theory]
+        [InlineData("abc-d@mail.com")]
+        [InlineData("abc.def@mail.com")]
+        [InlineData("abc@mail.com")]
+        [InlineData("abc_def@mail.com")]
+        [InlineData("abc.def@mail.cc")]
+        [InlineData("abc.def@mail-archive.com")]
+        [InlineData("abc.def@mail.archive.com")]
+        [InlineData("abc.def@mail.org")]
+        public void Email_IsValid_CasosSimples(string email)
+        {
+            Assert.True(Validators.EMail.IsValid(email));
+        }
+
+        [Theory]
+        //[InlineData("abc-@mail.com")] // HTML5 aceita
+        //[InlineData("abc..def@mail.com")] // HTML5 aceita
+        //[InlineData(".abc@mail.com")] // HTML5 aceita
+        //[InlineData("abc#def@mail.com")] // HTML5 aceita
+        [InlineData("abc.def@mail#archive.com")]
+        //[InlineData("abc.def@mail")] // HTML5 aceita
+        [InlineData("abc.def@mail..com")]
+        public void Email_IsValid_Violacoes(string email)
+        {
+            Assert.False(Validators.EMail.IsValid(email));
+        }
+
+        [Theory]
+        [InlineData("simple@example.com")]
+        [InlineData("very.common@example.com")]
+        [InlineData("abc@example.co.uk")]
+        [InlineData("disposable.style.email.with+symbol@example.com")]
+        [InlineData("other.email-with-hyphen@example.com")]
+        [InlineData("fully-qualified-domain@example.com")]
+        [InlineData("user.name+tag+sorting@example.com")]
+        [InlineData("example-indeed@strange-example.com")]
+        [InlineData("example-indeed@strange-example.inininini")]
+        public void Email_IsValid_Exemplo(string email)
+        {
+            Assert.True(Validators.EMail.IsValid(email));
+        }
+
+    }
+}
---
+++ Simple.Brazilian/Validators/EMail.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Simple.Brazilian.Validators
+{
+    /// <summary>
+    /// Validação de endereços de e-mail
+    /// </summary>
+    public static class EMail
+    {
+        // Usa a validação do HTML5
+        // Não usa a RFC5322
+        private static readonly string regexEmail = @"^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
+        private static readonly Regex rx = new Regex(regexEmail);
+        /// <summary>
+        /// Valida se o texto é uma e-mail válido
+        /// </summary>
+        public static bool IsValid(string email)
+        {
+            // a@a.co
+            if (email.Length < 6) return false;
+            // Impede ataque na validação do Regex
+            if (email.Length > 256) return false;
+            
+            if (!email.Contains("@")) return false;
+
+            return rx.IsMatch(email);
+        }
+
+    }
+}
