b25918180bccf2038c93292d33c49a098bbeb488
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-02-03 18:15:23 -0300

    Create NetStd10_Any.cs

---
+++ Simple.Brazilian/Compatibilidade/NetStd10_Any.cs
@@ -0,0 +1,18 @@
+﻿#if NETSTANDARD1_0
+using System;
+
+namespace Simple.Brazilian
+{
+    internal static class NetStd10_Any
+    {
+        public static bool Any(this string text, Func<char, bool> predicate)
+        {
+            foreach(char c in text)
+            {
+                if(!predicate(c)) return false;
+            }
+            return true;
+        }
+    }
+}
+#endif
