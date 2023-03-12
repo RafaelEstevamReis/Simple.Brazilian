cbc73aa72b2439d7bac7657452dc9f7c8ee6abb8
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-03-12 13:15:54 -0300

    Create Net20 Hashset equivalent

---
+++ Simple.Brazilian/Compatibilidade/Net20_HashSet.cs
@@ -0,0 +1,17 @@
+﻿#if NET20
+namespace System.Collections.Generic;
+internal class HashSet<T>
+{
+    Dictionary<T, bool> dicData;
+    public HashSet(IEnumerable<T> init)
+    {
+        dicData = new Dictionary<T, bool>();
+        foreach(var i in init) dicData.Add(i, false);
+    }
+
+    public int Count => dicData.Count;
+
+    internal bool Contains(T c)
+        => dicData.ContainsKey(c);
+}
+#endif
