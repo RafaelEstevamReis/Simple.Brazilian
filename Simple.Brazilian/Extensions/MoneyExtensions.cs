80cda7a7f4e5981cf0fd20040106a94166ba7217
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-11-06 19:07:08 -0300

    Refatora namespace

--- Simple.Brazilian/Extensions/MoneyExtensions.cs
+++ Simple.Brazilian/Extensions/MoneyExtensions.cs
@@ -1,18 +1,17 @@
-﻿namespace Simple.Brazilian
+﻿namespace Simple.Brazilian;
+
+/// <summary>
+/// Extensões para valores monetários
+/// </summary>
+public static class MoneyExtensions
 {
     /// <summary>
-    /// Extensões para valores monetários
+    /// Converte número em texto monetário usando Formatters.Numbers.ToBRL com casas decimais especificadas
     /// </summary>
-    public static class MoneyExtensions
-    {
-        /// <summary>
-        /// Converte número em texto monetário usando Formatters.Numbers.ToBRL com casas decimais especificadas
-        /// </summary>
-        /// <param name="value">Valor a ser convertido</param>
-        /// <param name="decimals">Casas decimais</param>
-        /// <returns>Texto convertido</returns>
-        public static string ToBRL(decimal value, int decimals = 2)
-            => Formatters.Numbers.ToBRL(value, decimals);
+    /// <param name="value">Valor a ser convertido</param>
+    /// <param name="decimals">Casas decimais</param>
+    /// <returns>Texto convertido</returns>
+    public static string ToBRL(decimal value, int decimals = 2)
+        => Formatters.Numbers.ToBRL(value, decimals);
 
-    }
 }
