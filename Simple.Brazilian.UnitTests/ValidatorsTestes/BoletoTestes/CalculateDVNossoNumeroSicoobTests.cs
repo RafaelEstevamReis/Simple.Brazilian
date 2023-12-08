53be61ffc94423c3af8c0a0c2709a325952c4ecb
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-12-08 16:27:43 -0300

    Adiciona CalculateDV_NossoNumero_Sicoob

---
+++ Simple.Brazilian.UnitTests/ValidatorsTestes/BoletoTestes/CalculateDVNossoNumeroSicoobTests.cs
@@ -0,0 +1,25 @@
+﻿using Simple.Brazilian.Validators;
+using Xunit;
+
+namespace Simple.Brazilian.UnitTests.ValidatorsTestes.BoletoTestes
+{
+    public class CalculateDVNossoNumeroSicoobTests
+    {
+        [Theory]
+        [InlineData("4423", "531413", 7630, '0')]
+        [InlineData("4423", "531413", 8115, '0')]
+        [InlineData("4423", "531413", 7772, '1')]
+        [InlineData("4423", "531413", 8040, '2')]
+        [InlineData("4423", "531413", 8066, '3')]
+        [InlineData("4423", "531413", 7499, '4')]
+        [InlineData("4423", "531413", 7666, '5')]
+        [InlineData("4423", "531413", 8139, '6')]
+        [InlineData("4423", "531413", 8135, '7')]
+        [InlineData("4423", "531413", 7568, '8')]
+        [InlineData("4423", "531413", 7773, '9')]
+        public void CalculaDV(string numCooperativa, string numCliente, int nossoNumeroBase, char expected)
+        {
+            Assert.Equal(expected, Boleto.CalculateDV_NossoNumero_Sicoob(numCooperativa, numCliente, nossoNumeroBase));
+        }
+    }
+}
--- Simple.Brazilian/Validators/Boleto.cs
+++ Simple.Brazilian/Validators/Boleto.cs
@@ -18,6 +18,15 @@ public static class Boleto
     public static char CalculateDV_Itau(string texto)
         => ModValidation.Calculate2121Mod10IndividualDigitsChar(texto);
 
+    public static char CalculateDV_NossoNumero_Sicoob(string numCooperativa, string numCliente, int nossoNumeroBase)
+    {
+        string linhaContatenada = numCooperativa.PadLeft(4, '0') +
+                                  numCliente.PadLeft(10, '0') +
+                                  nossoNumeroBase.ToString("0000000");
+
+        return CalculateDV_NossoNumero(linhaContatenada);
+    }
+
     /// <summary>
     /// Executa o cálculo do Fator de Vencimento com retorno em texto com 4 dígitos
     /// </summary>
@@ -39,5 +48,34 @@ public static class Boleto
         // O fator de vencimento é o total de dias entre
         // a data base e a data de vencimento
         return (dataVencimento - DATA_BASE).Days;
+    }
+
+    public static char CalculateDV_NossoNumero(string nossoNumero)
+    {
+        const string pattern = "319731973197319731973";
+        const int len = 21;
+
+        string linhaContatenada;
+
+        if (nossoNumero == null) throw new ArgumentNullException($"'{nameof(nossoNumero)}' não pode ser null");
+        else if (nossoNumero.Length == len) linhaContatenada = nossoNumero;
+        else if (nossoNumero.Length < len) linhaContatenada = nossoNumero.PadLeft(len, '0');
+        else throw new ArgumentException($"'{nameof(nossoNumero)}' não pode ser maior que {len}");
+        
+
+        int somatoria = 0;
+        for (int i = 0; i < pattern.Length; i++)
+        {
+            int num1 = linhaContatenada[i] - '0';
+            int num2 = pattern[i] - '0';
+
+            somatoria += num1 * num2;
+        }
+        int restoDivisao = somatoria % 11;
+
+        if (restoDivisao == 0) return '0';
+        if (restoDivisao == 1) return '0';
+
+        return (char)('0' + (11 - restoDivisao));
     }
 }
