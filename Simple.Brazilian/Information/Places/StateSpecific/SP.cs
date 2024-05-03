25412bb7f970a0e25f33c637ce3695b7e71d436b
Rafael Estevam <rafaelestevamreis@gmail.com>
2024-05-03 13:59:17 -0300

    Create SP.cs

---
+++ Simple.Brazilian/Information/Places/StateSpecific/SP.cs
@@ -0,0 +1,14 @@
+﻿namespace Simple.Brazilian.Information.Places.StateSpecific;
+
+using System;
+
+public class SP : IStateData
+{
+    public UFs UF => UFs.SP;
+
+    public bool IsSateHoliday(DateTime date, out string name)
+    {
+        name = null;
+        return false;
+    }
+}
