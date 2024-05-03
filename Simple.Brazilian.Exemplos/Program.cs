98558c2e8c036bbe6f8dbca4743b71b070ce6766
Rafael Estevam <rafaelestevamreis@gmail.com>
2024-05-03 13:50:59 -0300

    Converte para TopLevel

--- Simple.Brazilian.Exemplos/Program.cs
+++ Simple.Brazilian.Exemplos/Program.cs
@@ -1,16 +1,7 @@
 ﻿using System;
 
-namespace Simple.Brazilian.Exemplos
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Olá, mundo !");
+Console.WriteLine("Olá, mundo !");
 
-            //BenchmarkRunner.Run<Desempenho.AnalisaRemocaoAcentos>();
-            //BenchmarkRunner.Run<Desempenho.AnalisaAplicacaoMascara>();
-            //BenchmarkRunner.Run<Desempenho.AnalisaValidacaoDocumentos>();
-        }
-    }
-}
+//BenchmarkRunner.Run<Desempenho.AnalisaRemocaoAcentos>();
+//BenchmarkRunner.Run<Desempenho.AnalisaAplicacaoMascara>();
+//BenchmarkRunner.Run<Desempenho.AnalisaValidacaoDocumentos>();
